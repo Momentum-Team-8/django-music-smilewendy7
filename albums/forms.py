3d73b3881a0fcaed54267f9f0acfd412c14c0fd0
smilewendy7 <smilewendy7@gmail.com>
2021-06-18 23:46:31 -0400

    delete and edit is working

---
+++ albums/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Album
+
+
+class AlbumForm(ModelForm):
+    class Meta:
+        model = Album
+        fields = [
+            'title',
+            'artist',
+            'cover_art',
+        ]
+
--- django_music/urls.py
+++ django_music/urls.py
@@ -20,7 +20,14 @@ from albums import views as albums_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', albums_views.list_ablums, name='list_albums'),
+    path('', albums_views.list_albums, name='list_albums'),
+    path('albums/add/', albums_views.add_album, name='add_album'),
+    path('albums/<int:pk>/edit/',
+         albums_views.edit_album,
+         name='edit_album'),
+    path('albums/<int:pk>/delete/',
+         albums_views.delete_album,
+         name='delete_album'),
     
 ]
 
